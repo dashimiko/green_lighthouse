b6ebb3368fe5869fc3b75462a6ecffab33193e6b
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-08-22 17:30:52 +0400

    finish desktop version of help and organizers blocks

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,37 +1,32 @@
+
+const menuLinks = document.querySelectorAll('.header__list-link[data-goto]');//пункты меню хедера
+const navButton = document.querySelectorAll('.button');//кнопки
+const burgerMenu = document.querySelector(".hamburger-menu");//икнока бургера
+const burgerNavigation = document.querySelector(".header__navigation");//список пунктов меню хедера
+
 //плавный скролл к якорю
-const menuLinks = document.querySelectorAll('.header__list-link[data-goto]');
+function onMenuLinkClick(e) {
+  const menuLink = e.target;
+  if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+    const gotoBlock = document.querySelector(menuLink.dataset.goto);
+    const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 
-if(menuLinks.length > 0) {
-  menuLinks.forEach(menuLink => {
-    menuLink.addEventListener('click', onMenuLinkClick);
-  });
-
-  function onMenuLinkClick(e) {
-    const menuLink = e.target;
-    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-      const gotoBlock = document.querySelector(menuLink.dataset.goto);
-      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-
-      //закрытие меню после клика на пункт меню
-      if (burgerMenu.classList.contains('header__navigation_active')){
-        document.body.classList.remove('body_lock');
-        burgerMenu.classList.remove('header__navigation_active');
-        burgerNavigation.classList.remove('header__navigation_active');
-      }
-
-      window.scrollTo({
-        top: gotoBlockValue,
-        behavior: "smooth"
-      });
-      e.preventDefault();
+    //закрытие меню после клика на пункт меню
+    if (burgerMenu.classList.contains('header__navigation_active')) {
+      document.body.classList.remove('body_lock');
+      burgerMenu.classList.remove('header__navigation_active');
+      burgerNavigation.classList.remove('header__navigation_active');
     }
+
+    window.scrollTo({
+      top: gotoBlockValue,
+      behavior: "smooth"
+    });
+      e.preventDefault();
   }
 }
 
 //открытие и закрытие модалки меню для мобильных разрешений
-const burgerMenu = document.querySelector(".hamburger-menu");
-const burgerNavigation = document.querySelector(".header__navigation");
-
 if (burgerMenu) {
   burgerMenu.addEventListener('click', () => {
     document.body.classList.toggle('body_lock');
@@ -39,6 +34,18 @@ if (burgerMenu) {
     burgerNavigation.classList.toggle('header__navigation_active');
   })
 }
+
+//слушатель для скролла по клике на меню в хедере
+if(menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+  });
+}
+//слушатель для скролла по клику на кнопки
+if(navButton.length > 0)
+navButton.forEach(buttonLink => {
+  buttonLink.addEventListener('click', onMenuLinkClick);
+});
 
 //добавление класса активному блоку меню при скролле
 /*window.addEventListener('scroll', () => {
